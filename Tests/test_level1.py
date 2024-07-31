b80403cc2a75b6fe3220307585dcb30a1809f763
Sameer Shukla <sameershukla2@outlook.com>
2024-07-31 13:03:49 +0100

    Update test_level1.py

--- Tests/test_level1.py
+++ Tests/test_level1.py
@@ -1,12 +1,12 @@
 import unittest
 
-from level1 import solution
+from InvestIn Exercises import solution1
 
 
 class Level1TestCase(unittest.TestCase):
 
     def test_level1(self):
-        self.assertEqual(solution(), 'Hello World!')
+        self.assertEqual(solution1(), 'Hello World!')
 
 
 if __name__ == '__main__':
