2e7b788ba074c9985a4b6f58693a354a8cf9379b
Sameer Shukla <sameershukla2@outlook.com>
2024-07-31 13:04:26 +0100

    Update test_level2.py

--- Tests/test_level2.py
+++ Tests/test_level2.py
@@ -1,36 +1,36 @@
 import unittest
 
-from level2 import solution
+from InvestIn Exercises import solution2
 
 
 class Level2TestCase(unittest.TestCase):
 
     def test_level2isOdd(self):
-        self.assertEqual(solution(3), 'Weird')
+        self.assertEqual(solution2(3), 'Weird')
 
     def test_level2isNotOdd(self):
-        self.assertEqual(solution(4), 'Not Weird')
+        self.assertEqual(solution2(4), 'Not Weird')
 
     def test_level2isEveninRangeNotWeird(self):
-        self.assertEqual(solution(2), 'Not Weird')
+        self.assertEqual(solution2(2), 'Not Weird')
 
     def test_level2isNotEveninRangeNotWeird(self):
-        self.assertEqual(solution(5), 'Weird')
+        self.assertEqual(solution2(5), 'Weird')
 
     def test_level2isEveninRangeWeird(self):
-        self.assertEqual(solution(8), 'Weird')
+        self.assertEqual(solution2(8), 'Weird')
 
     def test_level2isNotEveninRangeWeird(self):
-        self.assertEqual(solution(15), 'Weird')
+        self.assertEqual(solution2(15), 'Weird')
 
     def test_level2isEveninRangeGreaterThan20(self):
-        self.assertEqual(solution(22), 'Not Weird')
+        self.assertEqual(solution2(22), 'Not Weird')
 
     def test_level2isNotEveninRangeGreaterThan20(self):
-        self.assertEqual(solution(23), 'Weird')
+        self.assertEqual(solution2(23), 'Weird')
 
     def test_level2isEveninRangeGreaterThan100(self):
-        self.assertEqual(solution(102), None)
+        self.assertEqual(solution2(102), None)
 
 
 if __name__ == '__main__':
